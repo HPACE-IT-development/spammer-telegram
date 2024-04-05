30806a0d8bcd353d377a3b4374e0f9725cb5ec6b
safo4eg <stas.rakov111@mail.ru>
2024-04-05 20:51:07 +0100

    refactoring rule

--- app/Rules/RecipientsList.php
+++ app/Rules/RecipientsList.php
@@ -8,20 +8,15 @@ use Illuminate\Support\Facades\Log;
 
 class RecipientsList implements ValidationRule
 {
-    /**
-     * Run the validation rule.
-     *
-     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
-     */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
        $recipients = explode(',', $value);
         foreach ($recipients as $recipient)
         {
-            if(!preg_match('/^https:\/\/t.me\/[A-z0-9\/]{5,32}(\/[0-9])*$/', trim($recipient)))
+            if(!preg_match('/^https:\/\/t.me\/[A-z0-9\/]{5,32}*$/', trim($recipient)))
             {
                 $fail('
-                    Каждый получатель(приватный чат, группа, подгруппа) должен быть корректной tg URLs скопированной из профиля и
+                    Каждый получатель(приватный чат, группа) должен быть корректной tg URLs скопированной из профиля и
                     отделённой от предыдущей запятой ",".
                 ');
             }
